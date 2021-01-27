edacef9db1d33d37498ecce63051039d9a7aa7fd
Ijay Abby <inyangasi4@gmail.com>
2021-01-27 15:26:16 +0300

    remove comma on line five in post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
     belongs_to:user
     has_many:comments
     validates:title, presence:true, uniqueness:true
-    validates:body, presence:true,
+    validates:body, presence:true
 end

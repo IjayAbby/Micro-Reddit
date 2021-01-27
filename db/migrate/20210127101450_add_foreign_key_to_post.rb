e2a9b0d51804a383fd45346af273f1fbd2791646
oluwadare juwon <wintan1418@gmail.com>
2021-01-27 11:15:29 +0100

    add user to task and add foreign keys

---
+++ db/migrate/20210127101450_add_foreign_key_to_post.rb
@@ -0,0 +1,4 @@
+class AddForeignKeyToPost < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_092118) do
+ActiveRecord::Schema.define(version: 2021_01_27_100011) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_092118) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

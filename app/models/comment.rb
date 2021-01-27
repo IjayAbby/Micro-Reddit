bc30aa1b7bc55a82580008750b237cd2feeb87be
oluwadare juwon <wintan1418@gmail.com>
2021-01-27 13:36:21 +0100

    run rubocop on code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    belongs_to:user
-    belongs_to:post
-    validates:comment, presence:true
+  belongs_to :user
+  belongs_to :post
+  validates :comment, presence: true
 end
